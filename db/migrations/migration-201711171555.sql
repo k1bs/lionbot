ecced89506e4b8942838108b624f22f29c561ae5
Alex Kibler <alexkibler@me.com>
2017-11-17 16:01:45 -0500

    create initial migration

---
+++ db/migrations/migration-201711171555.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS commands;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users_commands;
+
+CREATE TABLE IF NOT EXISTS commands (
+  id SERIAL PRIMARY KEY,
+  keyword VARCHAR(255) NOT NULL,
+  response TEXT NOT NULL,
+  default_command BOOLEAN NOT NULL,
+  enabled BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  password_digest TEXT NOT NULL,
+  email VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS users_commands (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER FOREIGN KEY REFERENCES users(id),
+  command_id INTEGER FOREIGN KEY REFERENCES commands(id)
+);
