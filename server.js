422041d67e2fd19f23e7fe8c7380491a6300f0c3
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-11-20 08:22:55 -0500

    Finished server.js setup

--- server.js
+++ server.js
@@ -13,19 +13,22 @@ app.use(logger('dev'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(cookieParser())
 
-app.use(
-  session({
-    key: process.env.SECRET_KEY,
-    secret: process.env.SECRET_KEY,
-    resave: false,
-    saveUninitialized: true
-  })
-)
+// Uncomment when auth is wired
 
-app.use(passport.initialize())
-app.use(passport.session())
+// app.use(cookieParser())
+//
+// app.use(
+//   session({
+//     key: process.env.SECRET_KEY,
+//     secret: process.env.SECRET_KEY,
+//     resave: false,
+//     saveUninitialized: true
+//   })
+// )
+//
+// app.use(passport.initialize())
+// app.use(passport.session())
 
 app.use(express.static('public'))
 
@@ -33,4 +36,30 @@ const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`🤖 Listening on port ${PORT}`)
+})
+
+app.get('/', (req,res) => {
+  res.send('🤖 LionBot is alive !')
+})
+
+// Uncomment when auth is wired
+
+// const authRoutes = require('./routes/auth-routes')
+// app.use('/api/auth', authRoutes)
+
+const commandRoutes = require('./routes/command-routes')
+app.use('/api/commands', commandRoutes)
+
+app.use('*', (req,res) => {
+  res.status(400).json({
+    message: 'Not Found!'
+  })
+})
+
+app.use((err, req, res, next) => {
+  console.log(err);
+    res.status(500).json({
+      error: err,
+      message: err.message
+    })
 })
