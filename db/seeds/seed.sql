add05c06140ef0bf31803c3dc30b90ac4dfb92b8
Alex Kibler <alexkibler@me.com>
2017-11-20 09:39:17 -0500

    add seed

---
+++ db/seeds/seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO commands (keyword, response, standard, enabled) VALUES ('!test', 'this is a test', true, true);
+INSERT INTO commands (keyword, response, standard, enabled) VALUES ('!alex', 'Whats up, alex?', true, true);
