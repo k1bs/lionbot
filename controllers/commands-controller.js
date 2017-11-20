6275bbf7bf2ff8b007d8288e3488d36fe9874d87
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-11-20 10:38:33 -0500

    Created findById method in commands controller

--- controllers/commands-controller.js
+++ controllers/commands-controller.js
@@ -5,9 +5,16 @@ const commandController = {}
 commandController.index = (req, res, next) => {
   Command.findAll()
     .then(commands => res.json({
-      message: 'ok!',
+      message: 'Ok!',
       data: {commands}
     }))
 }
 
+commandController.findById =  (req, res, next) => {
+  Command.findById(req.params.id)
+    .then(command => res.json({
+      message: 'Ok!',
+      data: {command}
+    }))
+}
 module.exports = commandController
