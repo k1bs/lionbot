8f6e36e1796b000576faaf8b5ce21c2270ab9417
Alex Kibler <alexkibler@me.com>
2017-11-20 13:45:11 -0500

    add logout method

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,19 @@ class App extends Component {
     }
     this.handleLoginSubmit = this.handleLoginSubmit.bind(this)
     this.handleRegisterSubmit = this.handleRegisterSubmit.bind(this)
+    this.logout = this.logout.bind(this)
+  }
+
+  componentDidMount () {
+    fetch('/api/auth/verify', {
+      credentials: 'include'
+    }).then(res => res.json())
+      .then(res => {
+        this.setState({
+          auth: res.auth,
+          user: res.data.user
+        })
+      }).catch(err => console.log(err))
   }
 
   handleLoginSubmit (e, data) {
