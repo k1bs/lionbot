7d324c61cfd3d74aa940639be5d44ec3a74637e1
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-11-20 10:36:25 -0500

    Created model method to select command by id

--- models/Command.js
+++ models/Command.js
@@ -4,8 +4,11 @@ const Command = {}
 
 Command.findAll = () => {
   return db.query(
-    `SELECT * FROM commands ORDER BY id ASC`
-  )
+    `SELECT * FROM commands ORDER BY id ASC`)
 }
 
+Command.findById = (id) => {
+  return db.One(
+    `SELECT * FROM commands WHERE id = $1`, id)
+}
 module.exports = Command
