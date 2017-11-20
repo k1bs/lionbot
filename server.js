55b3af4ca448afc2cab68fe7f167f60acf1febe4
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-11-20 08:17:04 -0500

    Added code for later auth usage, will comment out for now

--- server.js
+++ server.js
@@ -10,3 +10,27 @@ const app = express()
 require('dotenv').config()
 
 app.use(logger('dev'))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
+
+app.use(
+  session({
+    key: process.env.SECRET_KEY,
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(express.static('public'))
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`🤖 Listening on port ${PORT}`)
+})
