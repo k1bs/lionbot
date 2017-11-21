89634d95d8a4ef7f864ef1bd223aec76a1b59471
Alex Kibler <alexkibler@me.com>
2017-11-21 09:28:09 -0500

    comment db config

--- db/config.js
+++ db/config.js
@@ -1,11 +1,15 @@
+// Set options object
 const options = {
   query: (e) => {
+    // Console log every query
     console.log(e.query)
   }
 }
 
+// Import pg-promise, assign to variable pgp with options
 const pgp = require('pg-promise')(options)
 
+// Add conditional database setup for dev
 function setDatabase () {
   if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
     return pgp({
@@ -18,6 +22,7 @@ function setDatabase () {
   }
 }
 
+// Call pgp conditional function
 const db = setDatabase()
 
 module.exports = db
