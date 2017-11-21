10aacd8139ecc1fc0ba57e84b1670d7b4a513720
Alex Kibler <alexkibler@me.com>
2017-11-21 09:48:12 -0500

    comment passport.js

--- services/auth/passport.js
+++ services/auth/passport.js
@@ -1,12 +1,15 @@
+// Import passport and User model
 const passport = require('passport')
 const User = require('../../models/User')
 
+// Declare basic serialize and deserialize functions
 module.exports = () => {
   passport.serializeUser((user, done) => {
     done(null, user.username)
   })
 
   passport.deserializeUser((username, done) => {
+    // Calls to User table
     User.findByUserName(username)
       .then(user => {
         done(null, user)
