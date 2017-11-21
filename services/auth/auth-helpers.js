93944eff9c24c26e980b69bff7160abad43bea7c
Alex Kibler <alexkibler@me.com>
2017-11-21 09:36:42 -0500

    comment auth-helpers.js

--- services/auth/auth-helpers.js
+++ services/auth/auth-helpers.js
@@ -1,9 +1,14 @@
+// Import bcrypt
 const bcrypt = require('bcryptjs')
 
+// define comparpass
 function comparePass (userPassword, databasePassword) {
+  // use bcrypt's compareSync to match userpassword and databasePassword
+  // returns boolean
   return bcrypt.compareSync(userPassword, databasePassword)
 }
 
+// Export object containing method above
 module.exports = {
   comparePass: comparePass
 }
