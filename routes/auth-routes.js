933f6b5b746236998c56c409f25cf12343613a08
Alex Kibler <alexkibler@me.com>
2017-11-21 09:58:37 -0500

    comment auth-routes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,18 +1,23 @@
+// import express router, local strategy, and users controller
 const express = require('express')
 const authRouter = express.Router()
 const passport = require('../services/auth/local')
 const usersController = require('../controllers/users-controller')
 
+// on post requests to /register, user create method
 authRouter.post('/register', usersController.create)
 
+// on post requests to /login, user local strategy
 authRouter.post('/login', passport.authenticate('local', {
   successRedirect: '/api/auth/verify',
   failureRedirect: '/api/auth/verify',
   failureFlash: true
 }))
 
+// on get requests to verify, check for user status
 authRouter.get('/verify', (req, res) => {
   if (req.user) {
+    // if user, return auth-true and the user
     return res.status(200).json({
       message: 'ok',
       auth: true,
@@ -21,6 +26,7 @@ authRouter.get('/verify', (req, res) => {
       }
     })
   } else {
+    // otherwise, return auth-false
     return res.status(400).json({
       message: 'login failed',
       auth: false,
@@ -31,6 +37,7 @@ authRouter.get('/verify', (req, res) => {
   }
 })
 
+// on gets to /logout, return auth-false
 authRouter.get('/logout', (req, res) => {
   req.logout()
   res.json({
