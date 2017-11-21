cd9819dcd1de9acf19a0672aacc1669df7cd4b7d
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-11-21 10:25:40 -0500

    Fully commented command-routes file

--- routes/command-routes.js
+++ routes/command-routes.js
@@ -4,12 +4,15 @@ const express = require('express')
 // initializes router to wire paths accessible on our app
 const commandRouter = express.Router()
 
+// imports controller file to handle requests and respose data
 const commandController = require('../controllers/commands-controller')
 
+// detail of which controller method with handle certain requests to below paths
 commandRouter.get('/', commandController.index)
 commandRouter.get('/:id', commandController.findById)
 commandRouter.post('/', commandController.create)
 commandRouter.put('/:id', commandController.update)
 commandRouter.delete('/:id', commandController.delete)
 
+// export router file for use in server.js
 module.exports = commandRouter
