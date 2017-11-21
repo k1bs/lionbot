35ac935d7fc97a7bff70fdc5065f4718c674a004
Olivia Stoick <oliviastoick@gmail.com>
2017-11-21 15:36:52 -0500

    created about page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 // created basic footer with our AOA trademark - adding link to our about us page
 const Footer = () => {
   return (
     <footer>
-      <h2>Made by AOA Digital</h2>
+      <h2>Made by <Link to='/about'>AOA Digital</Link></h2>
     </footer>
   )
 }
