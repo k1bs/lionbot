6564224ec25c2db57f8db8e8f8aeaa57dfea53b7
Alex Kibler <alexkibler@me.com>
2017-11-22 11:01:47 -0500

    add a few tests

--- db/config.js
+++ db/config.js
@@ -9,11 +9,17 @@ const options = {
 // Import pg-promise, assign to variable pgp with options
 const pgp = require('pg-promise')(options)
 
-// Add conditional database setup for dev
+// Add conditional database setup for dev and test
 function setDatabase () {
   if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
     return pgp({
       database: 'lionbot_dev',
+      port: 5432,
+      host: 'localhost'
+    })
+  } else if (process.env.NODE_ENV === 'test') {
+    return pgp({
+      database: 'lionbot_test',
       port: 5432,
       host: 'localhost'
     })
