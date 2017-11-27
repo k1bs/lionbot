d7362da22af3a1f1d3a5bf289e46e6190f621216
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-11-27 15:04:05 -0500

    Added some styling to the main page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <footer>
-      <h2>Made by <Link to='/about'>AOA Digital</Link></h2>
+      <h2>Made by <span className='aoa_digital'><Link to='/about'>AOA Digital</Link></span></h2>
     </footer>
   )
 }
