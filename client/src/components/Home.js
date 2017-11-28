ed59b0fe790038816bad397323579d6fde77a57e
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-11-28 14:47:56 -0500

    Finished styling home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,25 +4,22 @@ import { Link } from 'react-router-dom'
 // created basic landing page
 const Home = () => {
   return (
-    <div className='home'>
       <div className='home'>
         <h1>Introducing, the King of all <span className='home_heading'>bots</span>.</h1>
-        <div className='chatBoxContainer'>
-          <div className='chatBox'>
-            <p>LionBot is a new way to improve your stream</p>
-          </div>
-          <div className='chatBox2'>
-            <p>With custom automated responses, you can focus on the game and let LionBot handle the chat.</p>
-          </div>
+
+        <div className='chatBox_logo_div'>
+          <p className='chatBox'><h3>LionBot is a new way to improve your stream</h3></p>
+          <p className='chatBox'><h3>With automated responses, you focus on gaming while LionBot handles the chat.</h3></p>
         </div>
-        <div id='lionbot_logo' />
+
+        <div id='lionbot_logo'></div>
 
         <ul>
           <li><Link to='/login'>Login</Link></li>
           <li><Link to='/register'>Register</Link></li>
         </ul>
+
       </div>
-    </div>
   )
 }
 
