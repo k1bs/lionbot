516d72e2dde80b4729de3654a622bb6ff6f15d86
Alex Kibler <alexkibler@me.com>
2018-01-09 11:40:53 -0500

    adjust styling of home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,24 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 // created basic landing page
 const Home = () => {
   return (
-    <div className='home'>
-      <h1>Introducing, the King of all <span className='home_heading'>bots</span>.</h1>
-
-      <div className='chatBox_logo_div'>
-        <p className='chatBox'><h3>LionBot is a new way to improve your stream</h3></p>
-        <p className='chatBox'><h3>With automated responses, you focus on gaming while LionBot handles the chat.</h3></p>
+    <div className='about'>
+      <div className='about-body'>
+        <h1>Introducing, the King of all <span className='home_heading'>bots</span>.</h1>
+        <div >
+          <h3>LionBot is a new way to improve your stream</h3>
+          <h3>With automated responses, you focus on gaming while LionBot handles the chat.</h3>
+        </div>
+        <div id='lionbot_logo' />
       </div>
-
-      <div id='lionbot_logo' />
-
-      <ul>
-        <li><Link to='/login'>Login</Link></li>
-        <li><Link to='/register'>Register</Link></li>
-      </ul>
-
     </div>
   )
 }
